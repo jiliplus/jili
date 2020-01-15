3527ff380fb84c56f043aa54a986bbf8fc5c3603
aQua <6028869+aQuaYi@users.noreply.github.com>
2020-01-15 14:03:55 +0800

    验证 time.AfterFunc 的运行方式。 果然，time.AfterFunc 返回的 timer.C 不会发送时间。

--- internal/playground/time/main.go
+++ internal/playground/time/main.go
@@ -7,23 +7,38 @@ import (
 )
 
 func main() {
-	passedTimer := time.NewTimer(-24 * time.Hour)
-	log.Println("ptc:", <-passedTimer.C)
-	tick := time.Tick(time.Millisecond * 500)
+
+	// passedTimer := time.NewTimer(-24 * time.Hour)
+	// log.Println("ptc:", <-passedTimer.C)
+	// tick := time.Tick(time.Millisecond * 500)
+	// var wg sync.WaitGroup
+	// wg.Add(10)
+	// go func() {
+	// 	for i := 0; i < 10; i++ {
+	// 		time.Sleep(time.Second)
+	// 		log.Println(<-tick)
+	// 		wg.Done()
+	// 	}
+	// }()
+	// wg.Wait()
+	// t := time.NewTimer(time.Second)
+	// t.Stop()
+	// log.Println(time.AfterFunc(time.Second, func() {
+	// 	log.Println("in AfterFunc")
+	// }))
+	// time.Sleep(time.Second)
+
+	log.Println("")
 	var wg sync.WaitGroup
-	wg.Add(10)
+	wg.Add(2)
+	timer := time.AfterFunc(time.Second*5, func() {
+		log.Println("in After")
+		wg.Done()
+	})
+	timer.Reset(time.Second * 2)
 	go func() {
-		for i := 0; i < 10; i++ {
-			time.Sleep(time.Second)
-			log.Println(<-tick)
-			wg.Done()
-		}
+		log.Println(<-timer.C)
+		wg.Done()
 	}()
 	wg.Wait()
-	t := time.NewTimer(time.Second)
-	t.Stop()
-	log.Println(time.AfterFunc(time.Second, func() {
-		log.Println("in AfterFunc")
-	}))
-	time.Sleep(time.Second)
 }

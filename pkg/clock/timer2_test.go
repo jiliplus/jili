14f2b8037fa762f35982ab5864b1974839f8a4c3
aQua <6028869+aQuaYi@users.noreply.github.com>
2020-01-18 14:32:50 +0800

    还在添加测试

--- pkg/clock/timer2_test.go
+++ pkg/clock/timer2_test.go
@@ -18,17 +18,16 @@ func Test_Simulator_Sleep(t *testing.T) {
 		duration := time.Second
 		s := NewSimulator(now)
 		Convey("并发地 s.Sleep(duration) 与 s.Move()", func() {
-			var wg sync.WaitGroup
-			var wakeUp time.Time
-			wg.Add(1)
-			// FIXME: 解决死锁问题
 			go func() {
-				s.Sleep(duration)
-				wakeUp = s.Now()
-				wg.Done()
+				// NOTICE: time.Sleep 是为了保证后面的程序被阻塞到 s.Sleep
+				// 但也不是 100% 的保证
+				time.Sleep(duration)
+				s.Move()
 			}()
-			s.Add(duration * 2)
-			wg.Wait()
+			s.Sleep(duration)
+			// 其实运行到这一步，已经说明了，可以从 Sleep 中恢复。
+			// 打到了 Sleep 的目的
+			wakeUp := s.Now()
 			Convey("Sleep 的完成时间，就是当前时间", func() {
 				So(wakeUp, ShouldEqual, s.Now())
 			})
@@ -62,9 +61,18 @@ func Test_Simulator_AfterFunc(t *testing.T) {
 		duration := time.Second
 		s := NewSimulator(now)
 		count := 0
+		// 由于 AfterFunc 是并发执行的，需要进行同步
+		// 同步的方法是
+		// 在 timer 的 set 前，添加 wg.Add(1)
+		// 在执行过的 timer.Reset 前，添加 wg.Add(1)
+		// 在 s.Add 后添加 wg.Wait()
+		var wg sync.WaitGroup
+		wg.Add(1)
 		timer := s.AfterFunc(duration, func() {
 			count++
+			wg.Done()
 		})
+
 		Convey("到达预定时间前", func() {
 			s.Add(duration / 2)
 			Convey("Func 不会被触发", func() {
@@ -79,22 +87,28 @@ func Test_Simulator_AfterFunc(t *testing.T) {
 				So(isActive, ShouldBeTrue)
 			})
 		})
+
 		Convey("到达预定时间后", func() {
 			s.Add(duration)
+			wg.Wait()
 			Convey("Func 会被触发一次", func() {
 				So(count, ShouldEqual, 1)
 			})
 			Convey("过段时间，重置一下", func() {
 				s.Add(duration)
+				// 已经执行过了，所以需要 wg.Add(1)
+				wg.Add(1)
 				isActive := timer.Reset(duration)
 				So(isActive, ShouldBeFalse)
 				Convey("到达预定时间后", func() {
 					s.Add(duration)
+					wg.Wait()
 					Convey("Func 会第 2 次被触发", func() {
 						So(count, ShouldEqual, 2)
 					})
 				})
 			})
 		})
+
 	})
 }

2db1cc13a36d710bd7787b7151715b6d7ba355e0
aQua <6028869+aQuaYi@users.noreply.github.com>
2020-04-15 22:20:16 +0800

    了解了 logrus 的运行过程

---
+++ internal/playground/logrus/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	// log.SetLevel(log.TraceLevel)
+	// log.Trace("trace logrus")
+	// log.Debug("debug logrus")
+	log.Info("info logrus")
+	// log.Warn("warn logrus")
+	// log.Error("error logrus")
+	// log.Fatal("fatal logrus")
+	// log.Panic("panic logrus")
+}

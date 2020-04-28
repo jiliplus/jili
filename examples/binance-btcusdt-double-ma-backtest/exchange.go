71a64d676bb499c382ab604128204e244c5f99ae
aQuaYi.com <6028869+aQuaYi@users.noreply.github.com>
2020-04-28 14:45:26 +0800

    添加了好多行

--- examples/binance-btcusdt-double-ma-backtest/exchange.go
+++ examples/binance-btcusdt-double-ma-backtest/exchange.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"log"
 	"math"
 
@@ -35,65 +37,117 @@ type pubsub interface {
 type order struct {
 	ID int64
 	// TODO: 把 OrderType 的 string 转变成枚举
-	OrderType       string
+	Type            string
 	Price, Quantity float64
-	Next, Prev      *order
+	Next            *order
 }
 
 func newOrder(id int64, orderType string, price, quantity float64) *order {
 	return &order{
-		ID:        id,
-		OrderType: orderType,
-		Price:     price,
-		Quantity:  quantity,
+		ID:       id,
+		Type:     orderType,
+		Price:    price,
+		Quantity: quantity,
 	}
 }
 
-type orders struct {
-	head, tail *order
+// TODO: 利用有限队列实现 sellOrders
+
+type sellOrders struct {
+	head *order
 }
 
-func newOrders() *orders {
+func newSellOrders() *sellOrders {
 	head := newOrder(math.MinInt64, "", -math.MaxFloat64, 0)
-	tail := newOrder(math.MaxInt64, "", math.MaxFloat64, 0)
-	head.Next = tail
-	tail.Prev = head
-	return &orders{head: head, tail: tail}
+	return &sellOrders{head: head}
 }
 
-func (o *orders) add(order *order) {
+func (o *sellOrders) push(order *order) {
 	curr, next := o.head, o.head.Next
-	for order.Price > next.Price {
+	for next != nil && order.Price > next.Price {
 		curr, next = next, next.Next
 	}
 	// now,
 	// order.Price <= next.Price
 	curr.Next, order.Next = order, next
-	next.Prev, order.Prev = order, curr
 }
 
-func (o *orders) cancel(id int64) {
-	curr := o.head.Next
-	for curr.ID != id {
+func (o *sellOrders) pop() *order {
+	if o.head.Next == nil {
+		return nil
+	}
+	res := o.head.Next
+	o.head.Next = o.head.Next.Next
+	res.Next = nil
+	return res
+}
+
+func (o *sellOrders) cancel(id int64) {
+	curr := o.head
+	for curr.Next != nil && curr.Next.ID != id {
 		curr = curr.Next
 	}
-	if curr == o.tail {
+	if curr.Next == nil {
 		return
 	}
-	curr.Next.Prev, curr.Prev.Next = curr.Prev, curr.Next
+	curr.Next = curr.Next.Next
 }
 
-func (o *orders) cancelAll() {
-	o.head.Next = o.tail
-	o.tail.Prev = o.head
+func (o *sellOrders) cancelAll() {
+	o.head.Next = nil
 }
 
-func (o *orders) () {
-	o.head.Next = o.tail
-	o.tail.Prev = o.head
+func (o *sellOrders) canSell(price float64) bool {
+	return o.head.Next.Price <= price
 }
 
+type buyOrders struct {
+	head *order
+}
 
+func newBuyOrders() *buyOrders {
+	head := newOrder(math.MinInt64, "", math.MaxFloat64, 0)
+	return &buyOrders{head: head}
+}
+
+func (o *buyOrders) push(order *order) {
+	curr, next := o.head, o.head.Next
+	for next != nil && order.Price < next.Price {
+		curr, next = next, next.Next
+	}
+	// now,
+	// order.Price >= next.Price
+	curr.Next, order.Next = order, next
+}
+
+func (o *buyOrders) pop() *order {
+	if o.head.Next == nil {
+		return nil
+	}
+	res := o.head.Next
+	o.head.Next = o.head.Next.Next
+	res.Next = nil
+	return res
+}
+
+func (o *buyOrders) cancel(id int64) {
+	curr := o.head
+	for curr.Next != nil && curr.Next.ID != id {
+		curr = curr.Next
+	}
+	if curr.Next == nil {
+		return
+	}
+	curr.Next = curr.Next.Next
+}
+
+func (o *buyOrders) cancelAll() {
+	o.head.Next = nil
+}
+
+func (o *buyOrders) canBuy(price float64) bool {
+	return o.head.Next.Price >= price
+}
 
 func newExchange(ctx context.Context, source <-chan tick, pubsub pubsub, initialMoney float64) *exchange {
 	exCtx, cancel := context.WithCancel(ctx)
@@ -110,12 +164,29 @@ func newExchange(ctx context.Context, source <-chan tick, pubsub pubsub, initial
 		if err != nil {
 			panic("pubsub.Subscribe(order) error" + err.Error())
 		}
-		orders := newOrders()
+		var orderBuf bytes.Buffer
+		orderDec := gob.NewDecoder(&orderBuf)
+
+		sellOrders, buyOrders := newSellOrders(), newBuyOrders()
 		for {
 			select {
 			case <-exCtx.Done():
 				log.Println("ctx done.")
-			case <-getOrder:
+			case msg := <-getOrder:
+				// TODO: 把这里相关的部分，写成闭包
+				orderBuf.Write(msg.Payload)
+				var order order
+				if err := orderDec.Decode(&order); err != nil {
+					log.Fatal("decode order error:", err)
+				}
+				switch order.Type {
+				case "sell":
+					sellOrders.push(&order)
+				case "buy":
+					buyOrders.push(&order)
+				default:
+					log.Fatal("出现了错误的 order type")
+				}
 			}
 		}
 	}()
